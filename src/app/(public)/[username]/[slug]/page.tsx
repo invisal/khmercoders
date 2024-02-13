9e8ef03fad41403c1adbb70345b7abd0cd134369
rin-yato <chearithorn@gmail.com>
2024-02-14 00:51:00 +0700

    feat: added view article page

---
+++ src/app/(public)/[username]/[slug]/page.tsx
@@ -0,0 +1,20 @@
+import { getArticleById } from "@/lib/query/article";
+
+import { Article } from "@/components/renderer";
+
+interface ArticlePageProps {
+  params: { username: string; slug: string };
+}
+export default async function ArticlePage(props: ArticlePageProps) {
+  const { slug } = props.params;
+  const article = await getArticleById(slug);
+
+  return (
+    <main className="p-8">
+      <Article
+        content={article.content}
+        className="prose prose-orange mx-auto"
+      />
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import { getUserAuth } from "@/lib/auth/utils";
+import { getAllArticles } from "@/lib/query/article";
 
 import ArticlePreview from "@/components/articles/article-preview";
 import HeroBanner from "@/components/hero-banner";
 import { Navbar } from "@/components/navbar";
 import ProfilePreview from "@/components/profile-preview";
 
+// revalidate on every request
+export const revalidate = 0;
+
 export default async function HomePage() {
   const session = await getUserAuth();
+  const articles = await getAllArticles();
+  console.log(articles);
 
   return (
     <div>
@@ -26,8 +32,20 @@ export default async function HomePage() {
           </div>
 
           <div className="mt-6 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-            {new Array(6).fill(true).map((_, idx) => {
-              return <ArticlePreview key={idx} />;
+            {articles.map(article => {
+              return (
+                <ArticlePreview
+                  key={article.id}
+                  id={article.id}
+                  title={article.title}
+                  description={article.description}
+                  cover={article.cover}
+                  author={{
+                    name: article.author.username,
+                    avatar: article.author.avatar,
+                  }}
+                />
+              );
             })}
           </div>
         </div>
