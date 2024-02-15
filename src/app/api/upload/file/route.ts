ff6c25144dc745aea1bbb2afa7e237b77c819337
rin-yato <chearithorn@gmail.com>
2024-02-16 01:46:04 +0700

    feat: add api route to upload by file and url

---
+++ src/app/api/upload/file/route.ts
@@ -0,0 +1,72 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { getUserAuth } from "@/lib/auth/utils";
+import { db } from "@/lib/db";
+import {
+  insertUserUploadSchema,
+  NewUserUpload,
+  userUploads,
+} from "@/lib/db/schema/user-uploads";
+import { md5 } from "@/lib/md5";
+import { R2 } from "@/lib/r2";
+import { concat } from "@/lib/utils";
+
+import { err, isErr, ok } from "@justmiracle/result";
+
+/**
+ * !IMPORTANT
+ *
+ * Make sure that you POST with form-data and the file key is "file"
+ */
+export const POST = async (request: NextRequest) => {
+  const { session } = await getUserAuth();
+  if (!session) {
+    return NextResponse.json("Unauthorized", { status: 401 });
+  }
+
+  const formData = await request.formData();
+  const file = formData.get("file") as File | undefined;
+
+  if (!file) {
+    return new Response("No file found", { status: 400 });
+  }
+
+  const size = file.size;
+  const fileType = file.type;
+  const fileExtenstion = file.type.split("/")[1] || "";
+  const hashedFilename = concat(md5(file.name), ".", fileExtenstion);
+
+  // we can do compression or resizing here and return a new buffer
+  const buffer = Buffer.from(await file.arrayBuffer());
+
+  const url = await R2.upload(buffer, hashedFilename, fileType)
+    .then(ok)
+    .catch(err);
+
+  if (isErr(url)) {
+    return NextResponse.json(url.error, { status: 400 });
+  }
+
+  const userUploadParams = insertUserUploadSchema.safeParse({
+    userId: session.user.id,
+    hashedFilename: hashedFilename,
+    filename: file.name,
+    size,
+  } satisfies NewUserUpload);
+
+  if (!userUploadParams.success)
+    return NextResponse.json(userUploadParams.error.message, { status: 400 });
+
+  const newUserUpload = await db
+    .insert(userUploads)
+    .values(userUploadParams.data)
+    .onConflictDoNothing()
+    .then(ok)
+    .catch(err);
+
+  if (isErr(newUserUpload)) {
+    return NextResponse.json(newUserUpload.error.message, { status: 400 });
+  }
+
+  return NextResponse.json({ url: url.value });
+};
---
+++ src/app/api/upload/url/route.ts
@@ -0,0 +1,71 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { getUserAuth } from "@/lib/auth/utils";
+import { db } from "@/lib/db";
+import {
+  insertUserUploadSchema,
+  NewUserUpload,
+  userUploads,
+} from "@/lib/db/schema/user-uploads";
+import { md5 } from "@/lib/md5";
+import { R2 } from "@/lib/r2";
+import { concat } from "@/lib/utils";
+
+import { err, isErr, ok } from "@justmiracle/result";
+
+export const POST = async (request: NextRequest) => {
+  const { session } = await getUserAuth();
+  if (!session) {
+    return NextResponse.json("Unauthorized", { status: 401 });
+  }
+
+  const body = await request.json();
+  const imageUrl = body.url;
+
+  if (!imageUrl || typeof imageUrl !== "string") {
+    return NextResponse.json("Invalid url", { status: 400 });
+  }
+
+  const fileResponse = await fetch(imageUrl);
+  const blob = await fileResponse.blob();
+
+  const filename = imageUrl.split("/").pop() || "";
+  const fileType = blob.type;
+  const fileExtenstion = fileType.split("/").pop() || "";
+  const size = blob.size;
+  const hashedFilename = concat(md5(filename), ".", fileExtenstion);
+
+  // we can do compression or resizing here and return a new buffer
+  const buffer = Buffer.from(await blob.arrayBuffer());
+
+  const url = await R2.upload(buffer, hashedFilename, fileType)
+    .then(ok)
+    .catch(err);
+
+  if (isErr(url)) {
+    return NextResponse.json(url.error, { status: 400 });
+  }
+
+  const userUploadParams = insertUserUploadSchema.safeParse({
+    userId: session.user.id,
+    hashedFilename: hashedFilename,
+    filename: filename,
+    size,
+  } satisfies NewUserUpload);
+
+  if (!userUploadParams.success)
+    return NextResponse.json(userUploadParams.error.message, { status: 400 });
+
+  const newUserUpload = await db
+    .insert(userUploads)
+    .values(userUploadParams.data)
+    .onConflictDoNothing()
+    .then(ok)
+    .catch(err);
+
+  if (isErr(newUserUpload)) {
+    return NextResponse.json(newUserUpload.error.message, { status: 400 });
+  }
+
+  return NextResponse.json({ url: url.value });
+};
