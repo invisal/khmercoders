a5bee3679560837f08b85ba61199709fc08096b1
chantepkosal <chantepkik@gmail.com>
2024-02-15 14:49:05 +0700

    feat : migrate to lottie-react

--- src/components/hero-banner/hero-banner-media.tsx
+++ src/components/hero-banner/hero-banner-media.tsx
@@ -4,18 +4,16 @@ import React from "react";
 
 import darkAnimationData from "./hero-media-dark.json";
 import lightAnimationData from "./hero-media-light.json";
-import Lottie from "lottie-react"; // Ensure correct import
+import Lottie from "lottie-react";
 import { useTheme } from "next-themes";
 
 export default function HeroBannerMedia() {
   const { theme, systemTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
 
-  // Determine which animation data to use based on the theme
   const animationData =
     currentTheme === "dark" ? darkAnimationData : lightAnimationData;
 
-  // Directly pass props to Lottie component instead of using an options object
   return (
     <div>
       <Lottie
