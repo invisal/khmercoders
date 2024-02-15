7363c91fbe1892dc25d93bb5bc52bc91004bd789
rin-yato <chearithorn@gmail.com>
2024-02-16 01:43:20 +0700

    feat: added more env

--- src/lib/env.mjs
+++ src/lib/env.mjs
@@ -8,8 +8,15 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+
     DATABASE_URL: z.string().min(1),
     DATABASE_AUTH_TOKEN: z.string().min(1),
+
+    R2_URL: z.string().min(1),
+    R2_PUBLIC_URL: z.string().min(1),
+    R2_BUCKET: z.string().min(1),
+    R2_ACCESS_KEY: z.string().min(1),
+    R2_SECRET_ACCESS_KEY: z.string().min(1),
   },
 
   client: {},
