748aeb277da7981e7b09c06459cc3bcc2dcce02e
chantepkosal <chantepkik@gmail.com>
2024-02-15 14:36:11 +0700

    adding lightmode lottie

--- src/components/hero-banner/hero-banner-media.tsx
+++ src/components/hero-banner/hero-banner-media.tsx
@@ -1,9 +1,20 @@
 "use client";
 
-import animationData from "./hero-media.json";
+import React from "react";
+
+import darkAnimationData from "./hero-media-dark.json";
+import lightAnimationData from "./hero-media-light.json";
+import { useTheme } from "next-themes";
 import Lottie from "react-lottie";
 
 export default function HeroBannerMedia() {
+  const { theme, systemTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  // Determine which animation data to use based on the theme
+  const animationData =
+    currentTheme === "dark" ? darkAnimationData : lightAnimationData;
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
