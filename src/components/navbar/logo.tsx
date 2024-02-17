868efff26b317c8489e0669502930bf8e90aa630
Thean <theantian089@gmail.com>
2024-02-17 16:21:45 +0700

    fix ci check 2.0 #4

--- src/components/navbar/logo.tsx
+++ src/components/navbar/logo.tsx
@@ -3,8 +3,6 @@ import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 
-import khmercoder from "public/khmercoder.svg";
-
 interface LogoProps extends React.HTMLAttributes<HTMLSpanElement> {
   iconSize?: number;
 }
@@ -16,7 +14,12 @@ export const Logo = ({ className, ...props }: LogoProps) => {
       {...props}
     >
       <Link href="/">
-        <Image src={khmercoder} alt="KhmercoderLogo" width={70} height={70} />
+        <Image
+          src="/khmercoder.svg"
+          alt="KhmercoderLogo"
+          width={70}
+          height={70}
+        />
       </Link>
     </span>
   );
