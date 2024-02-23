df08b0d9004823533b8c09a8bfeae70f6c66e986
Thean <theantian089@gmail.com>
2024-02-23 21:44:51 +0700

    #30 feat: add article metadata (maybe excessive)

--- src/app/(public)/[username]/[slug]/page.tsx
+++ src/app/(public)/[username]/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { Metadata } from "next";
+
 import { getArticleById } from "@/lib/query/article";
 import { getIdFromSlug } from "@/lib/utils";
 
@@ -7,6 +9,49 @@ interface ArticlePageProps {
   params: { username: string; slug: string };
 }
 
+export async function generateMetadata(
+  props: ArticlePageProps,
+): Promise<Metadata> {
+  const article = await getArticleById(getIdFromSlug(props.params.slug));
+  if (!article) {
+    throw new Error("Article not found");
+  }
+
+  const imageUrl = article.cover || "";
+  const author = article.author;
+
+  if (!author) {
+    return {};
+  }
+  return {
+    title: article.title,
+    description: article.description,
+    creator: author.username,
+    authors: [
+      {
+        name: author.username,
+        url: `${process.env.SITE_URL}/${author.username}` || "",
+      },
+    ],
+    publisher: "KhmerCoders",
+    openGraph: {
+      type: "article",
+      url: `${process.env.SITE_URL}/${author.username}/${article.slug}` || "",
+      images: [{ url: imageUrl, alt: article.title }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      images: [
+        {
+          url: imageUrl,
+          alt: article.title,
+          height: 630,
+          width: 1200,
+        },
+      ],
+    },
+  };
+}
 export default async function ArticlePage(props: ArticlePageProps) {
   const { slug } = props.params;
   const articleId = getIdFromSlug(slug);
