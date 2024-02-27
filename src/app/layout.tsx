7236f4e093e4b21141de9ca83fded4a626db0b9e
Visal .In <invisal@gmail.com>
2024-02-27 23:03:37 +0700

    feat: add google analytics (#56)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 
 import "./globals.css";
 
+import Script from "next/script";
+
 import { cn } from "@/lib/utils";
 
 import { Toaster } from "@/components/ui/sonner";
@@ -30,6 +32,20 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn(inter.className, "antialiased")}>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-CMZYJD5WQ8"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-CMZYJD5WQ8');
+          `}
+        </Script>
+
         <ThemeProvider enableSystem attribute="class">
           {children}
         </ThemeProvider>
